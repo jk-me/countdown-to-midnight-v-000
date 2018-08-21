7bd60d8a844f763764fae5003d90b0f045a643bc
Jenny K <jkam17@gmail.com>
2018-08-21 03:20:21 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,6 +4,14 @@ def countdown(n)
   while n>0 
     puts "#{n} SECOND(S)!"
     n-=1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown(n)
+  while n>0 
+    puts "#{n} SECOND(S)!"
+    n-=1
     sleep(1)
   end
   return "HAPPY NEW YEAR!"
